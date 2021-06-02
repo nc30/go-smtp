061cdb3807c05d8c9ffe8c650b1b09d29a1300bb
Shoji Imamura <m-imamura@goldkey.co.jp>
2021-06-02 17:09:57 +0900

    remove log system

--- conn.go
+++ conn.go
@@ -7,30 +7,24 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"net"
 	"net/textproto"
-	"os"
 	"regexp"
 	"runtime/debug"
 	"strconv"
 	"strings"
 	"sync"
-	"sync/atomic"
 	"time"
 )
 
 // Number of errors we'll tolerate per connection before closing. Defaults to 3.
 const errThreshold = 3
 
-var reqid uint64
-
 type ConnectionState struct {
 	Hostname   string
 	LocalAddr  net.Addr
 	RemoteAddr net.Addr
 	TLS        tls.ConnectionState
-	Conn       *Conn
 }
 
 type Conn struct {
@@ -54,9 +48,6 @@ type Conn struct {
 
 	fromReceived bool
 	recipients   []string
-
-	ID  string
-	Log *log.Logger
 }
 
 func newConn(c net.Conn, s *Server) *Conn {
@@ -97,15 +88,6 @@ func (c *Conn) init() {
 	}
 
 	c.text = textproto.NewConn(rwc)
-
-	if c.ID == "" {
-		myid := atomic.AddUint64(&reqid, 1)
-		c.ID = fmt.Sprintf("%s-%06d", c.server.Addr, myid)
-	}
-
-	if c.Log == nil {
-		c.Log = log.New(os.Stdout, c.ID+" ", log.Flags())
-	}
 }
 
 // Commands are dispatched to the appropriate handler functions.
@@ -230,7 +212,6 @@ func (c *Conn) State() ConnectionState {
 	state.Hostname = c.helo
 	state.LocalAddr = c.conn.LocalAddr()
 	state.RemoteAddr = c.conn.RemoteAddr()
-	state.Conn = c
 
 	return state
 }
@@ -647,8 +628,6 @@ func (c *Conn) handleStartTLS() {
 		c.SetSession(nil)
 	}
 	c.reset()
-
-	c.Log.Println("STARTTLS Successed")
 }
 
 // DATA
